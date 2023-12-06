e143cd6f7d8184fd85b8ecee746b17fb82549f3a
ferpuchaortiz <ferpuchaortiz@gmail.com>
2023-12-06 22:10:24 +1000

    reset

--- change_app/lib/main.dart
+++ change_app/lib/main.dart
@@ -1,68 +1,37 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatelessWidget{
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'ChangeApp',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.yellowAccent),
-        useMaterial3: true,
+      home: Scaffold(
+        body: BodyWidget(),
       ),
-      home: const MyHomePage(title: 'CHANGE'),
-    );
+);
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-
+class BodyWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center( ),
- // This trailing comma makes auto-formatting nicer for build methods.
+    return Align(
+      alignment: Alignment.center,
+      child: Text.rich(
+        TextSpan(
+          text: 'Hola',// defauld texto
+          children: <TextSpan>[
+            TextSpan(text: ' bellisimo ', style: TextStyle(fontStyle: FontStyle.italic)),
+            TextSpan(text: 'Mundo', style: TextStyle(fontWeight:  FontWeight.bold)),
+          ],
+        )
+        ),
     );
+    // TODO: implement build
+    throw UnimplementedError();
   }
+
 }
